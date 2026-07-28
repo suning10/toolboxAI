44dd6dd97e1a49aea63d99f06e43653f068ffc84
lqin61 <lqin61@gatech.edu>
2026-07-28 16:15:48 -0500

    fix router error

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 from app.api.v1.endpoints import chat, sops, upload
 
 api_router = APIRouter(prefix="/admin", tags=["admins"])
-api_router.include_router(upload.router, tags=["datasets"])
+# api_router.include_router(upload.router, tags=["datasets"])
 api_router.include_router(sops.router, tags=["knowledge"])
 api_router.include_router(chat.router, tags=["chat"])
--- main.py
+++ main.py
@@ -4,4 +4,4 @@ def main():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8081, reload=True, log_level="info")
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8081, log_level="error")
